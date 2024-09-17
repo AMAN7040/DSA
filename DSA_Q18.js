99fb39d899c430dfdf5279bdddf44a70aee2b01c
AMAN7040 <amansingh79620@gmail.com>
2024-09-17 18:43:35 +0530

    Q18

---
+++ DSA_Q18.js
@@ -0,0 +1,43 @@
+// MERGE SORT
+
+const merge = (arr, low, mid, high) => {
+  const temp = [];
+  left = low;
+  right = mid + 1;
+
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push(arr[left]);
+      left++;
+    } else {
+      temp.push(arr[right]);
+      right++;
+    }
+  }
+
+  while (left <= mid) {
+    temp.push(arr[left]);
+    left++;
+  }
+  while (right <= high) {
+    temp.push(arr[right]);
+    right++;
+  }
+
+  for (i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
+};
+
+const mergeSort = (arr, low, high) => {
+  if (low === high) return;
+
+  let mid = Math.floor((low + high) / 2);
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+  merge(arr, low, mid, high);
+};
+
+const arr = [2, 1, 4, 3];
+mergeSort(arr, 0, arr.length - 1);
+console.log(arr);
