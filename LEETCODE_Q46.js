034d99cd7b7d8019e335f137519f445624ddc9c7
AMAN7040 <amansingh79620@gmail.com>
2024-10-01 01:10:15 +0530

    rotate matrix 90 degree

---
+++ LEETCODE_Q46.js
@@ -0,0 +1,29 @@
+//48. Rotate Image
+
+const rotate = (matrix) => {
+  const n = matrix.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    let start = 0;
+    end = n - 1;
+    while (start < end) {
+      [matrix[i][start], matrix[i][end]] = [matrix[i][end], matrix[i][start]];
+      start++;
+      end--;
+    }
+  }
+  return matrix;
+};
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(rotate(matrix));
