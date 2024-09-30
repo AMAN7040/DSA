6e7b66ef86e1ea00b314e3dec36b806bb55bd195
AMAN7040 <amansingh79620@gmail.com>
2024-10-01 02:55:22 +0530

    subarray sum equals K

---
+++ LEETCODE_Q48.js
@@ -0,0 +1,25 @@
+//560. Subarray Sum Equals K
+
+const subSum = (nums,k) => {
+  let sum = 0;
+  let count = 0;
+  let hash = new Map();
+  hash.set(0,1);
+
+  for(let i=0; i<nums.length; i++){
+    sum += nums[i];
+    let rem = sum - k;
+
+    if(hash.has(rem)){
+        count += hash.get(rem);
+    }
+
+    hash.set(sum, (hash.get(sum) || 0) +1)
+  }
+  return count;
+}
+
+const arr = [1,2,3,-3,1,1,1,4,2,-3];
+const k = 3;
+
+console.log(subSum(arr,k));
